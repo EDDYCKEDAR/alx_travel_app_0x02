5b20af979d8d7bddd2cd1e8976980cd29e1e0138
Eddy Ckedar <eddykedar@gmail.com>
2025-08-16 18:51:54 +0000

    Create models.py

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Payment(models.Model):
+    booking_reference = models.CharField(max_length=100, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=20, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
